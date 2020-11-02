01b084c86596df156363e60000237f052385b82d
Berrouz777 <59357477+Berrouz777@users.noreply.github.com>
2020-11-03 00:47:10 +0500

    Реализовал слайдеры, попровил чекбоксы, сделал автопрефикс и минифицировал css и js

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,32 @@
 const clickWriteUs = document.querySelector(".show-modal");
-const clickMap = document.querySelector(".show-maps")
+const clickMap = document.querySelector(".show-maps");
 const modalWriteUs = document.querySelector(".modal-write-us");
 const modalShowMap = document.querySelector(".modal-map");
 const closeWriteUs = modalWriteUs.querySelector(".close-button");
 const closeMap = modalShowMap.querySelector(".close-button");
+const special = document.querySelector(".assortment-item-special");
+const specialTextOne = special.querySelector(".assortment-item-special-text:first-child");
+const specialTextTwo = special.querySelector(".assortment-item-special-text:nth-child(2)");
+const btnNext = special.querySelector(".button-next");
+const btnBack = special.querySelector(".button-back");
+btnNext.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  special.classList.remove("background-special-one");
+  special.classList.add("background-special-two");
+  specialTextOne.classList.add("none");
+  specialTextTwo.classList.remove("none");
+  btnBack.classList.remove("button-slider-active");
+  btnNext.classList.add("button-slider-active");
+})
+btnBack.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  special.classList.add("background-special-one");
+  special.classList.remove("background-special-two");
+  specialTextOne.classList.remove("none");
+  specialTextTwo.classList.add("none");
+  btnNext.classList.remove("button-slider-active");
+  btnBack.classList.add("button-slider-active");
+})
 clickWriteUs.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalWriteUs.classList.add("modal");
